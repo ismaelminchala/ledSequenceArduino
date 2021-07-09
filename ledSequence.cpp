9c4ba99e9797ece34606a279a39bed40b355bfa3
Ismael Minchala <ismaelminchala@ismaelminchala.lan>
2021-07-09 11:52:30 -0500

    Leds sequence without modularization

--- ledSequence.cpp
+++ ledSequence.cpp
@@ -1,8 +1,7 @@
-// Do not remove the include below
 #include "ledSequence.h"
 
 //#define length(array) (sizeof(array) / sizeof(*(array)));
-
+/************ + Constants & variables declaration + ************/
 const	int16_t LED1 = 50;            // Port address of LEDx
 const 	int16_t LED2 = 51;
 const 	int16_t LED3 = 52;
@@ -11,10 +10,16 @@ const 	int16_t KEY1 = 48;            // Port address of KEYx
 const 	int16_t KEY2 = 49;
 
 enum  	dir {ascending, descending};// Enum var for sequence
+typedef struct{
+	const int16_t* ptr;
+	const int16_t  len;
+	enum  dir      orientation;
+} sequenceControl;
+/***************************************************************/
 
 /****************** + Functions prototypes + *******************/
-bool turnOffLeds(int16_t* leds , int16_t len);
-bool ledSequence(int16_t leds[], int16_t len, dir);
+bool turnOffLeds(sequenceControl controller);
+bool ledSequence(sequenceControl controller);
 bool sequenceSpeed(int16_t tBase, int16_t tDelay);
 bool toogleState();
 /***************************************************************/
@@ -40,16 +45,15 @@ void loop() {
 			int16_t	t_delay = slow;
 			int16_t	arrayOfLeds[] = {LED1, LED2, LED3, LED4};
 			int16_t	length = sizeof(arrayOfLeds) / sizeof(int);
-			dir 	direction = ascending;	// orientation
+			sequenceControl ledsControl = {arrayOfLeds, length, ascending};
 	/***********************************************************/
 
 	while(1){
-//		if ( digitalRead(KEY1) == LOW ){
 		if ( readKey(KEY1) ){
 			if(toogleState()){
-				direction = ascending;
+				ledsControl.orientation = ascending;
 			} else{
-				direction = descending;
+				ledsControl.orientation = descending;
 			}
 		}
 
@@ -61,21 +65,15 @@ void loop() {
 			}
 		}
 
-		/*if ( !digitalRead(KEY2) == LOW ){
-			t_delay = fast;
-		} else{
-			t_delay = slow;
-		}*/
-
 		if ( sequenceSpeed(t_base, t_delay) ){
-			ledSequence(arrayOfLeds, length, direction);
+			ledSequence(ledsControl);
 		}
 	}
 }
 /***************************************************************/
 
 
-bool turnOffLeds(int16_t* leds, int16_t len){
+/*bool turnOffLeds(int16_t* leds, int16_t len){
 	for (int i = 0; i < len; ++i){
 //	    digitalWrite(leds[i], LOW);
 //	    if (digitalRead(leds[i])){
@@ -87,7 +85,7 @@ bool turnOffLeds(int16_t* leds, int16_t len){
 	return 1;
 }
 
-bool ledSequence(int16_t leds[], int16_t len, dir orientation){
+bool ledSequence(int16_t* leds, int16_t len, dir orientation){
 	static int16_t pos = 0;
 	if (!turnOffLeds(leds, len)){
 		return 0;
@@ -104,6 +102,38 @@ bool ledSequence(int16_t leds[], int16_t len, dir orientation){
 			--pos;
 	    } else{
 	      pos = len - 1;
+	    }
+	}
+	return 1;
+}*/
+
+bool turnOffLeds(sequenceControl controller){
+	for (int i = 0; i < controller.len; ++i){
+	    digitalWrite(controller.ptr[i], LOW);
+	    if (digitalRead(controller.ptr[i])){
+	    	return 0;
+	    }
+	}
+	return 1;
+}
+
+bool ledSequence(sequenceControl controller){
+	static int16_t pos = 0;
+	if (!turnOffLeds(controller)){
+		return 0;
+	}
+	digitalWrite(controller.ptr[pos], HIGH);
+	if (controller.orientation == ascending){
+		if (pos < controller.len - 1){
+			++pos;
+	    } else{
+	      pos = 0;
+	    }
+	} else{
+		if (pos > 0){
+			--pos;
+	    } else{
+	      pos = controller.len - 1;
 	    }
 	}
 	return 1;
---
+++ sequences.h
@@ -0,0 +1,15 @@
+/*
+ * sequences.h
+ *
+ *  Created on: Jul 9, 2021
+ *      Author: ismaelminchala
+ */
+
+#ifndef SEQUENCES_H_
+#define SEQUENCES_H_
+
+
+
+
+
+#endif /* SEQUENCES_H_ */
